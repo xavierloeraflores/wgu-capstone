7d4057b252b920115e1b14c2aa79340f762694ef
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-17 18:27:01 -0700

    Model.py: Read csv data

---
+++ model/model.py
@@ -0,0 +1,8 @@
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = read_csv('train.csv')
+print(data)
