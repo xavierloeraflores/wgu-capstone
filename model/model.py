71fc2815a69f1bdba96af5f895c84a877846e9e9
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-17 19:00:22 -0700

    Logistic Regression Model

--- model/model.py
+++ model/model.py
@@ -7,3 +7,17 @@ from sklearn.metrics import accuracy_score
 data = read_csv('train.csv')
 X = data['tweet']
 y = data['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+vectorizer = TfidfVectorizer() #Test: max_features=1000 
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+model = LogisticRegression()
+model.fit(X_train_vec, y_train)
+
+y_pred = model.predict(X_test_vec)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
