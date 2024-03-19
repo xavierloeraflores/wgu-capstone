64eb47700349f33a748cb284d713204d010ec736
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-19 11:54:06 -0700

    useContentFilter hook

---
+++ frontend/src/hooks/useContentFilter.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export function useContentFilter() {
+  const [isFiltered, setIsFiltered] = useState(true);
+  useEffect(() => {
+    function handleFiltered() {
+      setIsFiltered(true);
+    }
+    function handleNotFiltered() {
+      setIsFiltered(false);
+    }
+    window.addEventListener("filtered", handleFiltered);
+    window.addEventListener("not-filtered", handleNotFiltered);
+    return () => {
+      window.removeEventListener("filtered", handleFiltered);
+      window.removeEventListener("not-filtered", handleNotFiltered);
+    };
+  }, []);
+  return isFiltered;
+}
