a49ef39e8ff9a50792fa052650330365b481a3a7
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-19 12:35:18 -0700

    Post Component Hidden View close #5

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardContent } from "./ui/card";
 import { PostAvatar } from "./PostAvatar";
 import { cn } from "@/lib/utils";
+import { useContentFilter } from "@/hooks/useContentFilter";
 
 type PostProps = {
   text: string;
@@ -14,14 +15,17 @@ export const Post: React.FC<PostProps & { className?: string }> = ({
   filter,
   className = "",
 }) => {
+  const { isFiltered } = useContentFilter();
   return (
     <Card className={cn("min-w-96 max-w-md", className)}>
-      <CardHeader className="flex flex-row flex-nowrap gap-2">
+      <CardHeader className="flex flex-row flex-nowrap gap-2 border-b-2">
         <PostAvatar filter={filter} />
         <h1 className="text-lg font-semibold text-primary">{title}</h1>
       </CardHeader>
-      <CardContent>
-        <p className="">{text}</p>
+      <CardContent
+        className={isFiltered ? "overflow-hidden blur-xl filter" : ""}
+      >
+        <p className={isFiltered ? "select-none" : "select-text"}>{text}</p>
       </CardContent>
     </Card>
   );
