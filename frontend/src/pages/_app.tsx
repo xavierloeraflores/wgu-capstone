a3813a54e5806e60d4a282dd47f343440c4bacaf
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-18 23:29:41 -0700

    Implemented Toaster close #1

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 
 import "@/styles/globals.css";
 
@@ -12,6 +13,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
       <Component {...pageProps} />
+      <Toaster />
     </main>
   );
 };
