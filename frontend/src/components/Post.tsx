08696cc1f0286ac7affd48077d5e88e81e3bbdb9
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-19 00:07:55 -0700

    Post.tsx cn twmerge close #21

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,13 +1,14 @@
 import { Card, CardHeader, CardContent } from "./ui/card";
 import { PostAvatar } from "./PostAvatar";
+import { cn } from "@/lib/utils";
 
-export const Post = () => {
+export const Post:React.FC<{  className?: string }> = ({  className='' }) => {
   const title = "Post Title";
   const content =
     "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter";
   const filter = false;
   return (
-    <Card className="min-w-96 max-w-md">
+    <Card className={cn("min-w-96 max-w-md", className)}>
       <CardHeader className="flex flex-row flex-nowrap gap-2">
         <PostAvatar filter={filter} />
         <h1 className="text-lg font-semibold text-primary">{title}</h1>
