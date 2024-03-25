198795857856b28da06e829e39a47a7ed757768d
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-24 19:58:12 -0700

    About Xavier Page

---
+++ frontend/src/pages/xavier.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>About Xavier</title>
+        <meta name="description" content="About Xavier's background" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>About Xavier</h1>
+    </>
+  );
+}
