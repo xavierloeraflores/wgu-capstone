20eae86904180d276cd82d44316d8847860dab96
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-25 07:57:16 -0700

    Renamed isFiltered ->isNSFW

--- frontend/src/components/ContentFilterToggle.tsx
+++ frontend/src/components/ContentFilterToggle.tsx
@@ -5,17 +5,17 @@ import { useAtom } from "jotai";
 import { ContentFilterAtom } from "@/pages/_app";
 
 export const ContentFilterToggle: React.FC = () => {
-  const [ isFiltered, setIsFiltered ] = useAtom(ContentFilterAtom)
+  const [ isNSFW, setIsNSFW ] = useAtom(ContentFilterAtom)
 
   const toggleMode = () => {
-    setIsFiltered(!isFiltered);
+    setIsNSFW(!isNSFW);
   };
   return (
     <Toggle
       onClick={toggleMode}
       className="bg-transparent disabled:pointer-events-auto disabled:opacity-100 data-[state=on]:bg-transparent data-[state=on]:text-accent-foreground data-[state=on]:hover:bg-muted data-[state=on]:hover:text-muted-foreground"
     >
-      {!isFiltered ? "😀" : "🤬"}
+      {isNSFW ? "😀" : "🤬"}
     </Toggle>
   );
 };
