cbf316a47a7547c492a30c9519ecd31431b94f39
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-25 15:01:51 -0700

    Scrollable Feed

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -16,13 +16,12 @@ export const Feed: React.FC<FeedProps & { className?: string }> = ({
     className = "",
 }) => {
     return (
-        <ScrollArea className={cn("p-4 rounded-md border", className)}>
+        <ScrollArea className={cn("p-4 rounded-md border h-[75vh]", className)}>
             <div className="p-4">
-
-            <h4 className="mb-4  font-medium leading-none">Posts</h4>
-            {posts.map((post, idx) => (
-                <Post key={idx} {...post} className="mt-4" />
-                ))}
+                <h4 className="mb-4  font-medium leading-none">Posts</h4>
+                {posts.map((post, idx) => (
+                    <Post key={idx} {...post} className="mt-4" />
+                    ))}
                 </div>
         </ScrollArea>
     );
