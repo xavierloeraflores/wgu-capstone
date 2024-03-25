2ceb8d0ea3553c47a97bc39c8f0d788afb571105
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-24 19:59:12 -0700

    About Page

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About the Racism ML Model" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>About</h1>
+    </>
+  );
+}
