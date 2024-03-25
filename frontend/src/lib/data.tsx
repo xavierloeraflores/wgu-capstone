9d9ece72e273faff90ae1a6ef92dc23140eb4ee7
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-25 15:01:39 -0700

    Dummy Data

---
+++ frontend/src/lib/data.tsx
@@ -0,0 +1,72 @@
+export const postsData = [
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "Post Title",
+      isNSFW: false,
+      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+    {
+      title: "NSFW Post Title",
+      isNSFW: true,
+      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    },
+  ];
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,40 +1,8 @@
 import Head from "next/head";
-import { Post } from "@/components/Post";
 import { Feed } from "@/components/Feed";
+import { postsData } from "@/lib/data";
 
 export default function Home() {
-  const posts = [
-    {
-      title: "Post Title",
-      isNSFW: false,
-      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-    {
-      title: "NSFW Post Title",
-      isNSFW: true,
-      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-    {
-      title: "Post Title",
-      isNSFW: false,
-      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-    {
-      title: "NSFW Post Title",
-      isNSFW: true,
-      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-    {
-      title: "Post Title",
-      isNSFW: false,
-      text: "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-    {
-      title: "NSFW Post Title",
-      isNSFW: true,
-      text: "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
-    },
-  ];
 
   return (
     <>
@@ -43,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Racism ML Detector" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Feed posts={posts} />
+      <Feed posts={postsData} />
     </>
   );
 }
