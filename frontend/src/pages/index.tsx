1afb52ed680b2bef353ce5dacc7aeb1ae2830843
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-24 18:54:01 -0700

    FIX: imports blocking prod build

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import { Post } from "@/components/Post";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { ContentFilterToggle } from "@/components/ContentFilterToggle";
