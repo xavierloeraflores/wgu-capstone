b0d044a1bf6bfe761d0536fe9a6d321d549c8d8e
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-24 20:00:50 -0700

    Tech Stack Page

---
+++ frontend/src/pages/tech.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tech Stack</title>
+        <meta name="description" content="About the Project Tech Stack" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>The Tech Stack</h1>
+    </>
+  );
+}
