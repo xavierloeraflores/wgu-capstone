0870c1a8e0d006b75c76b5567ee8cd5ebfd8ca7a
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-25 14:52:35 -0700

    Feed Component close #4

---
+++ frontend/src/components/Feed.tsx
@@ -0,0 +1,29 @@
+import { Post } from "@/components/Post";
+import { cn } from "@/lib/utils";
+import { ScrollArea } from "./ui/scroll-area";
+
+type PostProps = {
+  text: string;
+  title: string;
+  isNSFW: boolean;
+};
+type FeedProps = {
+  posts: PostProps[];
+};
+
+export const Feed: React.FC<FeedProps & { className?: string }> = ({
+    posts,
+    className = "",
+}) => {
+    return (
+        <ScrollArea className={cn("p-4 rounded-md border", className)}>
+            <div className="p-4">
+
+            <h4 className="mb-4  font-medium leading-none">Posts</h4>
+            {posts.map((post, idx) => (
+                <Post key={idx} {...post} className="mt-4" />
+                ))}
+                </div>
+        </ScrollArea>
+    );
+};
