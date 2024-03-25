c6e06f4b38ea3ce54c00e6242dbe1d0bf8bc054b
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-03-25 08:22:04 -0700

    AlertDialog for ContentFilterChange close #8

--- frontend/src/components/ContentFilterToggle.tsx
+++ frontend/src/components/ContentFilterToggle.tsx
@@ -3,6 +3,7 @@
 import { Toggle } from "./ui/toggle";
 import { useAtom } from "jotai";
 import { ContentFilterAtom } from "@/pages/_app";
+import { AlertDialog, AlertDialogTrigger, AlertDialogCancel, AlertDialogAction, AlertDialogContent,AlertDialogHeader,AlertDialogTitle,AlertDialogDescription,AlertDialogFooter } from "./ui/alert-dialog";
 
 export const ContentFilterToggle: React.FC = () => {
   const [ isNSFW, setIsNSFW ] = useAtom(ContentFilterAtom)
@@ -11,11 +12,27 @@ export const ContentFilterToggle: React.FC = () => {
     setIsNSFW(!isNSFW);
   };
   return (
-    <Toggle
-      onClick={toggleMode}
-      className="bg-transparent disabled:pointer-events-auto disabled:opacity-100 data-[state=on]:bg-transparent data-[state=on]:text-accent-foreground data-[state=on]:hover:bg-muted data-[state=on]:hover:text-muted-foreground"
-    >
-      {isNSFW ? "😀" : "🤬"}
-    </Toggle>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Toggle
+          className="bg-transparent disabled:pointer-events-auto disabled:opacity-100 data-[state=on]:bg-transparent data-[state=on]:text-accent-foreground data-[state=on]:hover:bg-muted data-[state=on]:hover:text-muted-foreground"
+        >
+          {isNSFW ? "😀" : "🤬"}
+        </Toggle>
+      </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Change Content Filter</AlertDialogTitle>
+              <AlertDialogDescription>
+                <p>Are you sure you want to change the content filter?</p>
+              </AlertDialogDescription> 
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={toggleMode}>Change</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+
   );
 };
