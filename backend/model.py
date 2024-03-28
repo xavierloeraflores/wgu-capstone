ad65d15db8036e14ae5bca239e483f51dc87d8d3
xavierloeraflores <x.loera.tech@gmail.com>
2024-03-28 12:22:19 -0700

    pickle load model & vectorizer

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,12 @@
-from sklearn import svm
 import pickle
 
 model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 
 def classify_post(post):
-    return True
+    _post = vectorizer.transform([post])
+    prediction = model.predict(_post)
+    print(prediction)
+    return prediction
+
+classify_post("Test Text")
