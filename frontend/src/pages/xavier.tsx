50f43341c1c67a67453e6afcf26b5636ccc9b184
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-01 10:32:54 -0700

    Removed unused imports

--- frontend/src/pages/xavier.tsx
+++ frontend/src/pages/xavier.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { ButtonProps, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
