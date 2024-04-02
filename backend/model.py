9fb91d86a57ee3afc00803a56695bad09d3751a5
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-02 16:05:06 -0700

    Console outputs on loading model and vectorizor

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,9 @@
 import pickle
 
+print('Loading model and vectorizer...')
 model = pickle.load(open('model.pkl', 'rb'))
 vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+print('Model and vectorizer loaded.')
 
 def classify_post(post):
     _post = vectorizer.transform([post])
