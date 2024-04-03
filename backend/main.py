f508e00761905d1c4ac8030c3a44415eba62a4ea
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-02 20:39:08 -0700

    Post route implemented post model

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from model import classify_post, model_health
-
+from posts import get_all_posts, get_post_by_id, create_post
+from Models import Post
 app = FastAPI()
 
 
@@ -19,12 +20,12 @@ def classify(post: str):
 
 @app.get("/api/posts")
 def read_posts():
-    return {"posts": ["post1", "post2", "post3"]}
+    return get_all_posts()
 
 @app.get("/api/posts/{post_id}")
 def read_post(post_id: int):
-    return {"post_id": post_id}
+    return get_post_by_id(post_id)
 
 @app.post("/api/posts")
-def create_post():
-    return {"Message": "Post created successfully"}
+def create_post(post: Post):
+    return {"Message": "Post created successfully", "post": post}
--- backend/posts.py
+++ backend/posts.py
@@ -1,3 +1,5 @@
+from Models import Post
+
 def get_all_posts(page=0):
     if page == 0:
         return ["post01", "post02", "post03"]
@@ -6,5 +8,5 @@ def get_all_posts(page=0):
 def get_post_by_id(post_id):
     return {"post_id": post_id}
 
-def create_post(post):
+def create_post(post: Post):
     return {"Message": "Post created successfully", "post": post}
