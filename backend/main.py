7f30263fff5778f6c7db16a3f148fb3364964cbe
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-02 21:22:54 -0700

    CORS origins & config

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model import classify_post, model_health
 from posts import get_all_posts, get_post_by_id, create_post
 from Models import Post
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+    "http://localhost:8000",
+    "http://localhost:5000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api")
 def read_root():
