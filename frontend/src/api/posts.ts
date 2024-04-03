db8ac65644c935cad37e32c59abfa6d758730392
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-02 21:45:42 -0700

    Updated CreatePostResponse to handle errors

--- frontend/src/api/posts.ts
+++ frontend/src/api/posts.ts
@@ -3,18 +3,28 @@ const baseUrl = env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
 
 export interface CreatePostResponse {
   message: string;
-  post: {
+  post?: {
     text: string;
   };
+  error?: string;
 }
 export async function createPost(text: string): Promise<CreatePostResponse> {
-  const response = await fetch(`${baseUrl}/api/posts`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text }),
-  });
-  const result = (await response.json()) as CreatePostResponse;
-  return result;
+  try {
+    const response = await fetch(`${baseUrl}/api/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+
+    const result = (await response.json()) as CreatePostResponse;
+    return result;
+  } catch (error) {
+    console.error("Failed to create post");
+    return {
+      message: "Failed to create post",
+      error: "Failed to create post",
+    };
+  }
 }
