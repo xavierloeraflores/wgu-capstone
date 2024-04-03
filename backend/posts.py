065fa80b6b7a6418292b63f0fb224e9cb0814d4f
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-02 18:55:54 -0700

    posts controller functions

---
+++ backend/posts.py
@@ -0,0 +1,10 @@
+def get_all_posts(page=0):
+    if page == 0:
+        return ["post01", "post02", "post03"]
+    return ["post1", "post2", "post3"]
+
+def get_post_by_id(post_id):
+    return {"post_id": post_id}
+
+def create_post(post):
+    return {"Message": "Post created successfully", "post": post}
