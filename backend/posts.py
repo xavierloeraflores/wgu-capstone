893a56e25abd1bf59f00407df5c1d915893000dd
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-12 09:55:46 -0700

    Expanded dummy data

--- backend/posts.py
+++ backend/posts.py
@@ -17,11 +17,57 @@ dummy_post3 = {
     "isNSFW": False,
     "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
   }
+dummy_post4 = {
+    "id": 4,
+    "isNSFW": False,
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+
+dummy_post5 = {
+    "id": 5,
+    "isNSFW": True,
+    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+
+dummy_post6 = {
+    "id": 6,
+    "isNSFW": False,
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+dummy_post7 = {
+    "id": 7,
+    "isNSFW": False,
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+
+dummy_post8 = {
+    "id":8 ,
+    "isNSFW": True,
+    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+
+dummy_post9 = {
+    "id": 9,
+    "isNSFW": False,
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
+dummy_post10 = {
+    "id": 10,
+    "isNSFW": False,
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+  }
 
 dummy_posts =[
     dummy_post1,
     dummy_post2,
-    dummy_post3
+    dummy_post3,
+    dummy_post4,
+    dummy_post5,
+    dummy_post6,
+    dummy_post7,
+    dummy_post8,
+    dummy_post9,
+    dummy_post10,
 ]
 def get_all_posts(page=0):
     if page == 0:
