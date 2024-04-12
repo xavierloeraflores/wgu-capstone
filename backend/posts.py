0f3eaf2c57b6f963ee9f8d5be895dc7c7ef5fa33
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-12 11:47:13 -0700

    Updated dummy data

--- backend/posts.py
+++ backend/posts.py
@@ -4,58 +4,68 @@ import random
 dummy_post1 = {
     "id": 1,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic", "v1", "v2"]
   }
 
 dummy_post2 = {
     "id": 2,
     "isNSFW": True,
-    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic", "v1"]
   }
 
 dummy_post3 = {
     "id": 3,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic"]
   }
 dummy_post4 = {
     "id": 4,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": [ "v2"]
   }
 
 dummy_post5 = {
     "id": 5,
     "isNSFW": True,
-    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": []
   }
 
 dummy_post6 = {
     "id": 6,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": [ "v1", "v2"]
   }
 dummy_post7 = {
     "id": 7,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic", "v2"]
   }
 
 dummy_post8 = {
     "id":8 ,
     "isNSFW": True,
-    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "NSFW Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["v1", "v2"]
   }
 
 dummy_post9 = {
     "id": 9,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic", "v2"]
   }
 dummy_post10 = {
     "id": 10,
     "isNSFW": False,
-    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter"
+    "text": "Post Content: This is an example of post content that will appear in the post component. Posts can be up to 144 characters as originally designed by Twitter",
+    "tags": ["classic"]
   }
 
 dummy_posts =[
