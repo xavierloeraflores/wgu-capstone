99e4074d71270f472ed6ac657140444ea9776631
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-15 16:10:25 -0700

    format_second_dataset close #110

--- data/main.py
+++ data/main.py
@@ -11,14 +11,24 @@ def read_csv_data():
 def format_datasets(dataset1, dataset2):
     print("Formatting datasets")
     formatted_dataset1_X, formatted_dataset1_y = format_first_dataset(dataset1)
+    formatted_dataset2_X, formatted_dataset2_y = format_second_dataset(dataset2)
     print(formatted_dataset1_X)
     print(formatted_dataset1_y)
+    print(formatted_dataset2_X)
+    print(formatted_dataset2_y)
 
 def format_first_dataset(dataset):
     print("Formatting first dataset")
     X = dataset['tweet']
     raw_y = dataset['label']
     y = raw_y.map({1: 'offensive',  0: 'safe'})
+    return X,y 
+
+def format_second_dataset(dataset):
+    print("Formatting second dataset")
+    X = dataset['tweet']
+    raw_y = dataset['class']
+    y = raw_y.map({0: 'offensive', 1: 'offensive', 2: 'safe'})
     return X,y 
 
 ## Main
