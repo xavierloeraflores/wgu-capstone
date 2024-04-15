7a54cf1dd48a307837a77528966edf18ee0cd1a6
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-15 15:56:27 -0700

    read_csv_data close #108

--- data/main.py
+++ data/main.py
@@ -1,3 +1,9 @@
 from os import getenv
+from pandas import read_csv
 
 DB_CONNECTION = getenv('DB_CONNECTION')
+
+def read_csv_data():
+    data = read_csv('./datasets/train.csv')
+    data2 = read_csv('./datasets/labeled_data.csv')
+    return data, data2
