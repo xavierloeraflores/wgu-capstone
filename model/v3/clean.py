4b61afd22d765df10f3ab1d36e72207a5f794fb7
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-17 20:47:23 -0700

    remove_markup & emojis close #100, close #99

--- model/v3/clean.py
+++ model/v3/clean.py
@@ -3,7 +3,7 @@ import re
 
 stopword = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
-terms = ['user', 'rt', 'amp', 'via', '...']
+terms = ['user', 'link', 'rt', 'amp', 'via', '...']
 
 stemmer = nltk.SnowballStemmer('english')
 
@@ -31,6 +31,22 @@ def remove_emojis(text):
     text = regex_pattern.sub(r'', text)
     return text
 
+def remove_markdown(text):
+    result = re.sub("<[a][^>]*>(.+?)</[a]>", 'link ', text)
+    result = re.sub('ð', '', result) 
+    result = re.sub('â', '', result)
+    result = re.sub('&#x27;', "'", result) # apostrophe
+    result = re.sub('&quot;', '"', result) 
+    result = re.sub('&amp;', '&', result)
+    result = re.sub('&#x2F;', ' ', result)
+    result = re.sub('<p>', ' ', result)
+    result = re.sub('<i>', ' ', result) 
+    result = re.sub('&#62;', '', result)
+    result = re.sub('&gt;', "", result) # >
+    result = re.sub('&lt;', "", result) # <
+    result = re.sub("\n", '', result) # newline 
+    return result
+
 def remove_stopwords(text):
     text = [word for word in text.split(' ') if word not in stopword]
     text = " ".join(text)
@@ -45,8 +61,10 @@ def remove_punctuation(text):
     text = re.sub(r'[^\w\s]', '', text)
     return text
 
-def clean_data(text, stopwords=True, special_terms=True, stem=True, punctuation=True, emojis=True):
+def clean_data(text, markdown=True, stopwords=True, special_terms=True, stem=True, punctuation=True, emojis=True):
     text = text.lower()
+    if markdown:
+        text = remove_markdown(text)
     if emojis:
         text = remove_emojis(text)
     if punctuation:
