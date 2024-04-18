f89fd9d0fbd00592a1eefd59e5c2c303fd78c7b7
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-17 18:27:00 -0700

    remove_emojis() close #100

--- model/v3/clean.py
+++ model/v3/clean.py
@@ -1,10 +1,28 @@
 import nltk
+import re
 
 stopword = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 stemmer = nltk.SnowballStemmer('english')
 
-
+def remove_emojis(text):
+    regex_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  
+                               u"\U0001F300-\U0001F5FF"  
+                               u"\U0001F680-\U0001F6FF"  
+                               u"\U0001F1E0-\U0001F1FF"  
+                               u"\U00002500-\U00002BEF"  
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               "]+", flags=re.UNICODE)
+    text = regex_pattern.sub(r'', text)
+    return text
 
 def remove_stopwords(text):
     text = [word for word in text.split(' ') if word not in stopword]
@@ -18,6 +36,7 @@ def stem_text(text):
 
 def clean_data(text):
     text = text.lower()
+    text = remove_emojis(text)
     text = remove_stopwords(text)
     text = stem_text(text)
     return text
