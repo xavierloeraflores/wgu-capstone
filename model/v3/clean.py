6ec8df29c19fb091f3d99f37cdceca9149ff622b
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-17 19:19:11 -0700

    remove_special_terms

--- model/v3/clean.py
+++ model/v3/clean.py
@@ -3,7 +3,14 @@ import re
 
 stopword = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
+terms = ['user', 'rt', 'amp', 'via', '...']
+
 stemmer = nltk.SnowballStemmer('english')
+
+def remove_special_terms(text):
+    text = [word for word in text.split(' ') if word not in terms]
+    text = " ".join(text)
+    return text
 
 def remove_emojis(text):
     regex_pattern = re.compile("["
@@ -38,15 +45,17 @@ def remove_punctuation(text):
     text = re.sub(r'[^\w\s]', '', text)
     return text
 
-def clean_data(text, remove_stopwords=True, stem_text=True, remove_punctuation=True, remove_emojis=True):
+def clean_data(text, stopwords=True, special_terms=True, stem=True, punctuation=True, emojis=True):
     text = text.lower()
-    if remove_emojis:
+    if emojis:
         text = remove_emojis(text)
-    if remove_punctuation:
+    if punctuation:
         text = remove_punctuation(text)
-    if remove_stopwords:
+    if special_terms:
+        text = remove_special_terms(text)
+    if stopwords:
         text = remove_stopwords(text)
-    if stem_text:
+    if stem:
         text = stem_text(text)
     return text
 
