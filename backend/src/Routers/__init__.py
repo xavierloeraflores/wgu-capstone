9058b844b63e3bb55e60befdf0f3770e01e435b6
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-18 17:21:32 -0700

    model_router for #116

--- backend/src/Routers/__init__.py
+++ backend/src/Routers/__init__.py
@@ -1,1 +1,2 @@
 from Routers.posts_router import posts_router
+from Routers.model_router import model_router
---
+++ backend/src/Routers/model_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from Models import PostInput
+from model import classify_post, model_health
+
+model_router = APIRouter()
+
+@model_router.get("/api/model")
+def model_health_route():
+    return model_health()
+
+@model_router.post("/api/model/classify")
+def classify(post: PostInput):
+    return {"classification": classify_post(post.text)}
