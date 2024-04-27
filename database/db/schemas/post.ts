a1c76541efcfec56f9c8c43481c58837d1f5889c
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-26 19:24:59 -0700

    Added column tags to posts table close #123

--- database/db/schemas/post.ts
+++ database/db/schemas/post.ts
@@ -1,7 +1,15 @@
-import { pgTable, serial, text, boolean, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  boolean,
+  timestamp,
+  json,
+} from "drizzle-orm/pg-core";
 export const posts = pgTable("posts", {
   id: serial("id").primaryKey().notNull(),
   text: text("text"),
   isNSFW: boolean("is_nsfw").notNull().default(false),
   createdAt: timestamp("created_at").defaultNow(),
+  tags: json("tags").$type<string[]>().notNull().default([]),
 });
