1502bc3ec1404a815edc49364af6bfad3fa74831
xavierloeraflores <x.loera.tech@gmail.com>
2024-04-26 19:16:01 -0700

    Deleted tags table close #124

--- database/db/schemas/tags.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, pgEnum } from "drizzle-orm/pg-core";
-
-export const tagEnum = pgEnum("tag", ["training", "user", "v1", "v2"]);
-export const tags = pgTable("tags", {
-  id: serial("id").primaryKey().notNull(),
-  tag: tagEnum("tag").notNull(),
-});
