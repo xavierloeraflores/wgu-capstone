9729f68e68b68245781edf926d64fd513bf69d78
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-04-28 08:47:48 -0700

    tagDescriptions for #96

---
+++ frontend/src/lib/tags.ts
@@ -0,0 +1,8 @@
+export const tagDescriptions = {
+  user: "This post was created by a user who visited the site",
+  dataset:
+    "This post was sourced from the dataset used to train the machine learning model",
+  v1: "This post was flagged as offensive by version 1 of the machine learning model",
+  v2: "This post was flagged as offensive by version 2 of the machine learning model",
+  v3: "This post was flagged as offensive by version 3 of the machine learning model",
+};
