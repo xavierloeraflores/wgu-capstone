d796faaba569186469e25be92bbee65864b116ee
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-05-03 10:20:15 -0700

    DataBarChart component

---
+++ frontend/src/components/DataBarChart.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { cn } from "@/lib/utils";
+import {
+  BarChart,
+  Tooltip,
+  Bar,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
+
+import { type BarChartDataPoint } from "@/types";
+
+type BarChartProps = {
+  data: BarChartDataPoint[];
+  xAxisLabel?: string;
+  yAxisLabel?: string;
+};
+
+export const DataBarChart: React.FC<BarChartProps & { className?: string }> = ({
+  data,
+  className = "",
+  xAxisLabel = "",
+  yAxisLabel = "",
+}) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart
+        id="data-bar-chart"
+        className={cn("text-black", className)}
+        data={data}
+        width={300}
+      >
+        <XAxis dataKey="label" stroke="#888888" />
+        <YAxis stroke="#888888" />
+        <Tooltip
+          formatter={(value) => [value, yAxisLabel]}
+          labelFormatter={(label) => `${xAxisLabel}: ${label}`}
+          contentStyle={{ backgroundColor: "white", opacity: 0.9 }}
+        />
+
+        <Bar
+          dataKey="value"
+          radius={[4, 4, 0, 0]}
+          fill="currentColor"
+          className="fill-primary"
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
