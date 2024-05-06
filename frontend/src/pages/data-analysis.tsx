ae678f4f7b80d6d3cf4fa1a42ac716abd02ee3d4
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-05-06 08:31:56 -0700

    implemented DataHeader component

--- frontend/src/pages/data-analysis.tsx
+++ frontend/src/pages/data-analysis.tsx
@@ -1,6 +1,5 @@
 import { DataCardRow } from "@/components/DataCardRow";
 import { DataTable } from "@/components/DataTable";
-import Image from "next/image";
 import { DataBarChart } from "@/components/DataBarChart";
 import {
   tableHeader,
@@ -8,40 +7,20 @@ import {
   testData,
   dataDropdown,
 } from "@/lib/data-analysis";
-import { DownloadDropdown } from "@/components/DownloadDropdown";
+import { DataHeader } from "@/components/DataHeader";
 
 export default function DataAnalysis() {
   return (
     <>
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
-            <div className="space-y-4">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                Unlock Insights into the training data used to train the model
-              </h1>
-              <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">
-                Explore the analysis of the training data or download any of the
-                raw data files.
-              </p>
-              <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                <DownloadDropdown
-                  menuTrigger={dataDropdown.menuTrigger}
-                  menuLabel={dataDropdown.menuLabel}
-                  menuItems={dataDropdown.menuItems}
-                />
-              </div>
-            </div>
-            <Image
-              alt="Emoji icon representing data analysis with a bar chart"
-              className="mx-auto aspect-video overflow-hidden rounded-xl object-bottom sm:w-full lg:order-last lg:aspect-square"
-              height="150"
-              width="150"
-              src="/images/chart.png"
-            />
-          </div>
-        </div>
-      </section>
+      <DataHeader
+        title="Unlock Insights into the training data used to train the model"
+        headline="Explore the analysis of the training data or download any of the raw data files."
+        dropdown={dataDropdown}
+        image={{
+          src: "/images/chart.png",
+          alt: "Emoji icon representing data analysis with a bar chart",
+        }}
+      />
 
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container grid gap-8 px-4 md:px-6">
