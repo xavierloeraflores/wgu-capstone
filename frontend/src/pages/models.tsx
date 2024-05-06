db18ada65f876ae1fe3a5b5abf29a25fa3d99211
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-05-06 08:37:48 -0700

    implemented DataHeader component

--- frontend/src/pages/models.tsx
+++ frontend/src/pages/models.tsx
@@ -1,43 +1,21 @@
-import { DownloadDropdown } from "@/components/DownloadDropdown";
 import { DataCardRow } from "@/components/DataCardRow";
 import { DataTable } from "@/components/DataTable";
-import Image from "next/image";
 import { DataBarChart } from "@/components/DataBarChart";
 import { tableHeader, tableRows, testData, modelsDropdown } from "@/lib/models";
+import { DataHeader } from "@/components/DataHeader";
 
 export default function DataAnalysis() {
   return (
     <>
-      <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container px-4 md:px-6">
-          <div className="grid items-center gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
-            <div className="space-y-4">
-              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                Learn more about the models and their performance.
-              </h1>
-              <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">
-                Explore the analysis of the different models or download model
-                pickle files.
-              </p>
-              <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                <DownloadDropdown
-                  menuTrigger={modelsDropdown.menuTrigger}
-                  menuLabel={modelsDropdown.menuLabel}
-                  menuItems={modelsDropdown.menuItems}
-                />
-              </div>
-            </div>
-            <Image
-              alt="Emoji icon representing machine learning with a robot face"
-              className="mx-auto aspect-video overflow-hidden rounded-xl object-bottom sm:w-full lg:order-last lg:aspect-square"
-              height="150"
-              width="150"
-              src="/images/robot.png"
-            />
-          </div>
-        </div>
-      </section>
-
+      <DataHeader
+        title="Learn more about the models and their performance."
+        headline="Explore the analysis of the different models or download model pickle files."
+        dropdown={modelsDropdown}
+        image={{
+          src: "/images/robot.png",
+          alt: "Emoji icon representing machine learning with a robot face",
+        }}
+      />
       <section className="w-full py-12 md:py-24 lg:py-32">
         <div className="container grid gap-8 px-4 md:px-6">
           <DataCardRow cards={dataCards}></DataCardRow>
