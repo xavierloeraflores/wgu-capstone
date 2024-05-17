35e963da8989cca268b68c8cbbdf8e057b57f25c
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-05-17 04:44:15 -0700

    updated drizzle-kit

--- database/drizzle.config.ts
+++ database/drizzle.config.ts
@@ -4,8 +4,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schemas/*",
   out: "./drizzle",
-  driver: "pg", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+  dialect: "postgresql", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    connectionString: process.env.DB_CONNECTION ?? "",
+    url: process.env.DB_CONNECTION ?? "",
   },
 } satisfies Config;
